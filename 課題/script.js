d91a3460e681271fd5c363eaedc4e0fadf2ed419
Masa1312-dig <152838173+Masa1312-dig@users.noreply.github.com>
2024-01-22 08:40:40 +0900

    Update script.js

--- 課題/script.js
+++ 課題/script.js
@@ -15,42 +15,6 @@ function test(actual, expected) {
 
 test()
 
-
-// let card = 1;// 場にあるトランプのカード番号
-
-// let Hi_L = 0;// Low(0) と High(1) どっちを選んだか仮の数字（実際にはクリックで決める）
-// let Result = "●●を選んで、あなたの●●";//勝ち負けの結果
-
-
-// function High_Low(Hi_L) {
-
-//       document.getElementById("Card_img_Before").src="torannpu-illust" + card + ".png";
-
-// let trump = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,]; // ハイアンドローをするカード番号
-// let trump_n = Math.floor( Math.random() * 13) +1 ;// ランダムで数字を決める+１で一回とる
-
-
-// if (card < trump[trump_n]) {//賭けカードが、伏せカードより大きい
-//   if (Hi_L === 0){ Result = "  LOWを選んで、あなたの<span style='background:blue;'>『 負け 』</span>";
-//       }else if(Hi_L === 1){ Result = "  HIGHを選んで、あなたの<span style='background:red;'>【 勝ち 】</span>";}
-// }
-// else if (card > trump[trump_n]) {//賭けカードが、伏せカードより小さい
-//   if (Hi_L === 0){ Result = "  LOWを選んで、あなたの<span style='background:red;'>【 勝ち 】</span>";
-//       }else if (Hi_L === 1){ Result = "  HIGHを選んで、あなたの<span style='background:blue;'>『 負け 』</span>";}
-// }
-// else {Result = "引き分け！！";}
-
-//       document.getElementById("Card_img_After").src="torannpu-illust" + trump_n + ".png";
-
-// document.getElementById("After").innerHTML ="伏せカードは" + trump[trump_n] + Result + "<br />次のカードが今の数字より高いか低いか考えてみよう！";
-// document.getElementById("Before").innerHTML ="場にあるカードは" + card ;
-
-// card = trump[trump_n];//場にあるカードを新しく引いたカードに変える
-
-// }
-
-
-
 let card = 1;// 場にあるトランプのカード番号
 
 let Hi_L = 0;//どっちを選んだか仮の引数　クリックして中身を変える
